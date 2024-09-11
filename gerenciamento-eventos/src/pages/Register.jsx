31bd26bcfac0690a1744f6df7d7939d241408e2a
IgorOliverx <igorferoli019@gmail.com>
2024-09-11 16:54:13 -0300

    Up da api funcionando corretamente e iniciando codagem do front-end

--- gerenciamento-eventos/src/pages/Register.jsx
+++ gerenciamento-eventos/src/pages/Register.jsx
@@ -4,8 +4,11 @@ export const Register = () => {
     return (
         <>
             <p className='text-2xl w-screen text-center mt-5'>Cadastro de Usuário</p>
-            <form action="" className='w-1/4 h-1/3 bg-white flex relative'>
+            <form action="" method='POST' className='w-2/4 gap-3 flex-wrap h-1/3 rounded-md py-5 bg-white flex relative mx-auto justify-center'>
                 <Input type='text' placeholder='Nome' name='nome' id='nome'/>
+                <Input type='email' placeholder='E-mail' name='email' id='email'/>
+                <Input type='password' placeholder='Senha' name='senha' id='senha'/>
+                <button type='submit' className='primary-btn'>Cadastrar</button>
             </form>
         </>
     )
