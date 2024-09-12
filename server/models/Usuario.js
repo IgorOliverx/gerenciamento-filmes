1659b7232db2bb8f956800ba0da49324d95e79c4
Paiva8859 <gabrielaguilarpaiva@gmail.com>
2024-09-12 14:38:49 -0300

    Usuário, registro (com criptografia) e autenticação

--- server/models/Usuario.js
+++ server/models/Usuario.js
@@ -1,19 +1,39 @@
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt"); // Importar bcrypt
 
 const UsuarioSchema = new mongoose.Schema({
-    nome: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    senha: {
-        type: String,
-        required: true
-    }
+  nome: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  senha: {
+    type: String,
+    required: true,
+  },
 });
 
-module.exports = mongoose.model('Usuario', UsuarioSchema);
+// Hash a senha antes de salvar
+UsuarioSchema.pre("save", async function (next) {
+  // Alterado para "save"
+  if (!this.isModified("senha")) {
+    // Verificar "senha" minúsculo
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.senha = await bcrypt.hash(this.senha, salt); 
+  next();
+});
+
+// Método para comparar senhas
+UsuarioSchema.methods.compararSenha = function (candidateSenha) {
+  return bcrypt.compare(candidateSenha, this.senha); 
+};
+
+const Usuario =
+  mongoose.models.Usuario || mongoose.model("Usuario", UsuarioSchema);
+
+module.exports = Usuario;
